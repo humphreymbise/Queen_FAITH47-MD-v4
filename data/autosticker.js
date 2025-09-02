9fd11281e56e8d7789542462a07a04f29c643239
Humphrey47 <humphreymbise68@gmail.com>
2025-09-02 10:16:48 +0300

    Update and rename autosticker.js to call_handler.js

--- data/autosticker.js
+++
@@ -1,7 +0,0 @@
-{
-  "bye": "https://github.com/humphreymbise/Queen_FAITH47-MD-v4-DATA/raw/main/autosticker/STK-20241108-WA0028.webp",
-  "love": "https://github.com/humphreymbise/Queen_FAITH47-MD-v4-DATA/raw/main/autosticker/STK-20241110-WA0007.webp",
-  "criss": "https://github.com/humphreymbise/Queen_FAITH47-MD-v4-DATA/raw/main/autosticker/STK-20241110-WA0008.webp",
-  "vevo": "https://github.com/humphreymbise/Queen_FAITH47-MD-v4-DATA/raw/main/autosticker/STK-20241110-WA0004.webp",
-  "hi": "https://github.com/humphreymbise/Queen_FAITH47-MD-v4-DATA/raw/main/autosticker/STK-20241110-WA0002.webp"
-}
